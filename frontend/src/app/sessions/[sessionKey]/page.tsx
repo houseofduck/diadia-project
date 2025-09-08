28e16a9c7e9851436930222ba12e9c48848a0210
Sam Ducker <sam@luckyduck.digital>
2025-09-08 13:11:17 +0100

    add sensitive files to gitignore and setup backend

--- frontend/src/app/sessions/[sessionKey]/page.tsx
+++ frontend/src/app/sessions/[sessionKey]/page.tsx
@@ -5,6 +5,7 @@ type Props = {
 };
 
 export default async function SessionPage({ params }: Props) {
+  // See https://nextjs.org/docs/messages/sync-dynamic-apis why await here
   const { sessionKey } = await params;
   return (
     <div className="h-full min-h-screen w-full flex items-center justify-center">
